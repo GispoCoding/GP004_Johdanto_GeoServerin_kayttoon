119bc3a24ac8574f37a998d070978dea72f762fb
Raine Ekman <raine@gispo.fi>
2025-10-24 13:41:01 +0300

    Chown syntax

--- geoserver-init.sh
+++ geoserver-init.sh
@@ -5,4 +5,4 @@ cd Helsinki.osm.shp
 for ext in dbf prj shp shx ; do mv gis.osm_buildings_a_free_1.$ext buildings.$ext ; done
 
 cp -a /koulutus $GEOSERVER_DATA_DIR
-chown -R geoserveruser.geoserverusers $GEOSERVER_DATA_DIR/koulutus
+chown -R geoserveruser:geoserverusers $GEOSERVER_DATA_DIR/koulutus
